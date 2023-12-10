73d743f5576c851502e27f891d84449001660277
Robin Schmid <roob1nch@gmail.com>
2023-12-10 16:52:01 +0100

    Added single view per category

--- app/category.py
+++ app/category.py
@@ -13,3 +13,20 @@ def index():
     ).fetchall()
 
     return render_template('category/index.html', categories = categories)
+
+@bp.route('/<int:id>')
+@login_required
+def single(id):
+    db = get_db()
+
+    category = db.execute('SELECT * FROM category WHERE id = ?', (id,)).fetchone()
+
+    positions = db.execute(
+            'SELECT p.id, p.text, p.created_at, ROUND(p.amount_rappen / 100.0, 2) AS amount, a.title AS account'
+            ' FROM position p'
+            ' JOIN account a ON p.account_id = a.id'
+            ' WHERE p.category_id = ?'
+            ' ORDER BY p.created_at DESC;', (category['id'],)).fetchall()
+    
+    return render_template('category/single.html', category = category, positions = positions)
+
